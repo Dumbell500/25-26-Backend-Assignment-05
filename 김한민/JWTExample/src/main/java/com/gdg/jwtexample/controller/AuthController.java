a83b09be96048bc30e71b2d0942f751caab16997
Kim, Hanmin <kimhanmin0601@gmail.com>
2025-11-23 18:52:49 +0900

    refactor: AuthController에 Lombok으로 생성자 주입 정리

--- 김한민/JWTExample/src/main/java/com/gdg/jwtexample/controller/AuthController.java
+++ 김한민/JWTExample/src/main/java/com/gdg/jwtexample/controller/AuthController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    private final AuthService authService;
 
-    public AuthController(AuthService authService) { this.authService = authService; }
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody @Valid SignupRequest req) {
